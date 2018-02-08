7b2de89f725de4ed04a5f460d13932f09c60d8d2
Gin <mambo06@gmail.com>
2018-02-08 18:12:20 +1100

    add file

---
+++ factorial.py
@@ -0,0 +1,19 @@
+def factorial(n):
+    factr = 1
+    while (n >0):
+        factr *= n
+        n -= 1
+
+def recursive_factorial(n):
+   if n == 1 :
+       return 1
+   return n * recursive_factorial(n-1)
+
+# print(recursive_factorial(5))
+
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(3))
